a6c898334b5a7df9b94bd03daf6244113db48bd2
Iago Bomfim Santana <121451583+IagoSantana1@users.noreply.github.com>
2026-07-26 16:52:17 +0000

    feat(batch): add logic to send messages to the logs channel

--- internal/commands/batch_process.go
+++ internal/commands/batch_process.go
@@ -1,13 +1,45 @@
 package commands
 
-import "fmt"
+import (
+	"EverythingSuckz/fsb/internal/utils"
+	"fmt"
+
+	"github.com/gotd/td/tg"
+)
+
+type CopiedMedia struct {
+	MessageID int
+	Message   *tg.Message
+}
 
 func processBatch(chatID int64, items []BatchItem) {
 
-	fmt.Println("-----------------------------------")
-	fmt.Println("Novo lote")
-	fmt.Println("Chat:", chatID)
-	fmt.Println("Arquivos:", len(items))
-	fmt.Println("-----------------------------------")
+	if len(items) == 0 {
+		return
+	}
 
+	fmt.Printf("Processando lote com %d arquivos\n", len(items))
+
+	mediaItems := make([]utils.MediaCopyItem, 0, len(items))
+
+	for _, item := range items {
+		msg := item.Update.EffectiveMessage
+
+		mediaItems = append(mediaItems, utils.MediaCopyItem{
+			Media:   msg.Media,
+			Caption: msg.Message.Message,
+		})
+
+	}
+
+	updates, err := utils.SendMediaCopy(items[0].Ctx, chatID, mediaItems)
+
+	if err != nil {
+		utils.Logger.Sugar().Error(err)
+		return
+	}
+
+	fmt.Printf("Telegram retornou %d updates\n", len(updates.Updates))
 }
+
+func ParseMediaUpdates(updates *tg.Updates) ([]CopiedMedia, error)
